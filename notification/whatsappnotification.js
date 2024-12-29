d17214e5cd38b9966229e1bb263c7c0631649b71
Vikram Kumar <vikram.choudhary@recqarz.com>
2024-12-29 16:47:06 +0530

    Added the function of sms,whatsapp,email to send the notication to user for next hearing

--- notification/whatsappnotification.js
+++ notification/whatsappnotification.js
@@ -1,25 +1,32 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
-const messageType = process.env.messageType
-const fromNumber = process.env.fromNumber
-const templateid = process.env.templateid
-const serviceType = process.env.serviceType
-const messageuuid = process.env.messageuuid
+const messageType = process.env.messageType;
+const fromNumber = process.env.fromNumber;
+const templateid = process.env.templateid_notification;
+const serviceType = process.env.serviceType;
+const messageuuid = process.env.messageuuid;
 
-export const sendWhatsappMessage = async (whatsappnumber, otp) => {
+export const sendWhatsappMessage = async (
+  caseTitled,
+  previousHearingDate,
+  cnrNumber,
+  hearingDate,
+  mobile
+) => {
+  let variable = `${caseTitled},${hearingDate},${cnrNumber},${previousHearingDate}`;
   const formData = new FormData();
   formData.append("messageType", messageType);
   formData.append("fromNumber", fromNumber);
-  formData.append("contactnumber", whatsappnumber);
+  formData.append("contactnumber", mobile);
   formData.append("templateid", templateid);
   formData.append("serviceType", serviceType);
-  formData.append("messageuuid", messageuuid);
+  formData.append("messageuuid", `${Date.now()}${messageuuid}`);
   formData.append("buttonValues", "");
   formData.append("dynamicUrl", "");
   formData.append("dynamicUrl2", "");
-  formData.append("message", otp);
+  formData.append("message", variable);
   const url = `https://automate.nexgplatforms.com/api/v1/wa/save-message`;
   try {
     const response = await fetch(url, {
