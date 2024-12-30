a7d400a7e0f6e289d24792e460cdb5bf6d159fc6
Vikram Kumar <vikram.choudhary@recqarz.com>
2024-12-30 12:12:18 +0530

    minor update

--- notification/cronjobnotification.js
+++ notification/cronjobnotification.js
@@ -1,11 +1,14 @@
 import cron from "node-cron";
-import { notificationSender06am, notificationSender11am } from "./notificationforhearing.js";
+import {
+  notificationSender06am,
+  notificationSender11am,
+} from "./notificationforhearing.js";
 
 export function sendHearingNotification() {
-  cron.schedule("* 11 * * *", () => {
+  cron.schedule("0 11 * * *", () => {
     notificationSender11am();
   });
-  cron.schedule("* 06 * * *", () => {
+  cron.schedule("0 06 * * *", () => {
     notificationSender06am();
   });
 }
